77f00647cda1b709e7d432602d2c8e37411faa80
yqrong <37606499+yqrong@users.noreply.github.com>
2018-04-24 14:35:38 +0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui'
+
+import store from './store.js'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/font-awesome-4.7.0/css/font-awesome.min.css'
+import '@/assets/css/style.css'
+
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+
+new Vue({
+  router,
+  store,
+  el: '#app',
+  render: h => h(App)
+})
---
+++ src/store.js
@@ -0,0 +1,36 @@
+/**
+ * Created by yqr on 2018/3/26.
+ */
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+/*测试数据*/
+const date = 'Mon Mar 24 2018 00:00:00 GMT+0800 (中国标准时间)'
+const data = [
+  {
+    id: '1111',
+    name: 'Allen',
+    type: '员工',
+    status: '已离职'
+  },{
+    id: '2222',
+    name: 'Thomas',
+    type: '司机',
+    status: '在职'
+  }
+]
+
+const state = {
+  activeList: data
+}
+
+/*从本地存储读取数据*/
+for(var item in state) {
+  localStorage.getItem(item)? state[item] = JSON.parse(localStorage.getItem(item)): false;
+}
+
+export default new Vuex.Store({
+  state
+})
