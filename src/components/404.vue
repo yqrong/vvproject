757e047b386967424723417117ac60cbbe4d7eb2
yqrong <37606499+yqrong@users.noreply.github.com>
2018-04-24 14:45:53 +0800

    Create 404.vue

---
+++ src/components/404.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg">
+    <p>404 NOT FOUNT o(╯□╰)o</p>
+  </div>
+</template>
+<style>
+  html,body {
+    height: 100%;
+  }
+  #app {
+    height: 100%;
+    margin-top: 0;
+  }
+</style>
