a0914eefe5035dda6284609ddfdd5bd39a1626ad
yqrong <37606499+yqrong@users.noreply.github.com>
2018-04-24 14:36:43 +0800

    Add files via upload

---
+++ src/api/api_user.js
@@ -0,0 +1,16 @@
+/**
+ * Created by yqr on 2018/4/13.
+ */
+import * as API from './'
+
+export default {
+  //登录
+  login: params => {
+    return API.POST('/api/users/login', params)
+  },
+  //登出
+  logout: params => {
+    return "success";
+    //return API.GET('/api/users/logout', params)
+  }
+}
